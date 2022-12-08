269a8e4f51281e5ed19a46c567196df16330f577
mattyonder <mateussantosor5002@gmail.com>
2022-12-08 00:28:41 -0300

    Sessão de estudos 00:28 08/12/2022

--- Estudos C#/Models/Pessoa.cs
+++ Estudos C#/Models/Pessoa.cs
@@ -7,12 +7,17 @@ namespace Estudos_C_.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Idade { get; set; } 
+        public string? NomeRepresentanteLegalPessoaFisica { get; set; }
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade}");
+            Console.WriteLine($"Olá, meu nome é \n{Nome}, e tenho {Idade}");
         }
     }
 }
+
+//PascalCase em Classes, propriedades e métodos
+//Não abreviar nomes de propriedades
+//Nome do arquivo deve ser o nome da classe
--- Estudos C#/Program.cs
+++ Estudos C#/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Estudos_C_.Models;
+
+//Pega a data atual
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.70;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+//ToString no altura para fazer o valor double ser mostrado com aquela formatação escrita
+
+
+
+
+
+
+
+
+
+
+
+/*Pessoa pessoa = new Pessoa();
+
+pessoa.Nome = "Mateus";
+pessoa.Idade = 17;
+pessoa.Apresentar();
+
+Pessoa pessoaFisica = new Pessoa();
+
+pessoaFisica.Nome = "Teteu";
+pessoaFisica.Idade = 17;
+pessoaFisica.Apresentar();
+*/
+
+//Nome de variavel em camelCase 
+//Sem caracter especial em classe, variavel ou propriedade
+//Underline é uma exceção
